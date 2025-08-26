5767dd3ab52ba3ebf76b17b1ad29cfe257aa9c7e
LoneWolf_108 <121552564+Saptarshi-108@users.noreply.github.com>
2025-08-26 22:27:07 +0530

    Update Picture.jsx

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -121,119 +121,120 @@ export default function Picture() {
 
 const itemData = [
   {
-    img: "src/pictures/avtar.jpg",
+    img: "/pictures/avtar.jpg",
     title: "Team Avatar",
     author: "Some of my favourite characters from ATLA",
   },
   {
-    img: "src/pictures/Batman.png",
+    img: "/pictures/Batman.png",
     title: "Batman",
     author: "The Dark knight",
   },
   {
-    img: "src/pictures/bobross.png",
+    img: "/pictures/bobross.png",
     title: "Bob Ross",
     author: "One of my favourite artists... Bob Ross",
   },
   {
-    img: "src/pictures/BobRossArt.png",
+    img: "/pictures/BobRossArt.png",
     title: "The Mountains",
     author: "From Bob Ross",
   },
   {
-    img: "src/pictures/cool.jpg",
+    img: "/pictures/cool.jpg",
     title: "The Cool guy",
     author: "Doodling in boring classes...",
   },
   {
-    img: "src/pictures/ACvalhalla.png",
+    img: "/pictures/ACvalhalla.png",
     title: "Viking warrior",
     author: "Take us to Valhalla!",
   },
   {
-    img: "src/pictures/dark is the night.png",
+    img: "/pictures/dark is the night.png",
     title: "Dark is the Night",
     author: "A Soviet Rifleman smoking in the trench",
   },
   {
-    img: "src/pictures/ghibli howling castle .jpg",
+    img: "/pictures/ghibli howling castle .jpg",
     title: "Ghibli Howl's Castle",
     author: "from Howl's Moving Castle (Hayao Miyazaki)",
   },
   {
-    img: "src/pictures/IMG_20250516_202742.jpg",
+    img: "/pictures/IMG_20250516_202742.jpg",
     title: "Solitary Knight",
     author: "Lonely knight mourning his fallen brothers.",
   },
   {
-    img: "src/pictures/Kedarnath.png",
+    img: "/pictures/Kedarnath.png",
     title: "Kedarnath Mandir",
     author: "Om namah shivay (ॐ नमः शिवाय)",
   },
   {
-    img: "src/pictures/IMG_20250711_191225.jpg",
+    img: "/pictures/IMG_20250711_191225.jpg",
     title: "H.P. Lovecraft",
     author: "Father of Cosmic Horror",
   },
   {
-    img: "src/pictures/old and young.jpg",
+    img: "/pictures/old and young.jpg",
     title: "Old and Young",
     author: "Am I bored?",
   },
   {
-    img: "src/pictures/robot.jpg",
+    img: "/pictures/robot.jpg",
     title: "Smokin' Hot",
     author: "This gladiator has a hot sword.LITERALLY.",
   },
   {
-    img: "src/pictures/shiva.png",
+    img: "/pictures/shiva.png",
     title: "Mahadev",
     author: "Har Har Mahadev",
   },
   {
-    img: "src/pictures/sketch.jpg",
+    img: "/pictures/sketch.jpg",
     title: "Portrait sketches",
     author: "Sketching for practice",
   },
   {
-    img: "src/pictures/takehiko inoue.jpg",
+    img: "/pictures/takehiko inoue.jpg",
     title: "Takehiko Inoue",
     author: "Author and Illustrator of Vagabond",
   },
   {
-    img: "src/pictures/SatyajitRay.png",
+    img: "/pictures/SatyajitRay.png",
     title: "Satyajit Ray",
     author: "Eminent Artist,Oscar winning Film-Director,Author.",
   },
   {
-    img: "src/pictures/Ronin.jpg",
+    img: "/pictures/Ronin.jpg",
     title: "Lone Samurai",
     author: "Two Swords - One for man, another for monsters.",
   },
     {
-    img: "src/pictures/Budhha.jpg",
+    img: "/pictures/Budhha.jpg",
     title: "Budhha in bliss",
     author: "Buddham Saranam Gachami",
   },
     {
-    img: "src/pictures/Ghost.jpg",
+    img: "/pictures/Ghost.jpg",
     title: "Ghost (COD)",
     author: "Lt. Simon 'Ghost' Riley (TF141)",
   },
     {
-    img: "src/pictures/IMG_20250826_193049.jpg",
+    img: "/pictures/IMG_20250826_193049.jpg",
     title: "Don't mess with me",
     author: "Sketch of a bearded guy.",
   },
       {
-    img: "src/pictures/TheJourney.jpg",
+    img: "/pictures/TheJourney.jpg",
     title: "The Journey",
     author: "Journey through open barren lands",
   },
         {
-    img: "src/pictures/darkcorridoor.jpg",
+    img: "/pictures/darkcorridoor.jpg",
     title: "The Dark Corridor",
     author: "Dark Corridor in a Hospital (Pic.)",
   },
 ];
+
 
